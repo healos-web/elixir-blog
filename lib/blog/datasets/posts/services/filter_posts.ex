6d7a614af6646e0299e2b314eb187ede35502af8
Maks <maks.shitko@mail.ru>
2020-12-03 01:25:49 +0300

    rewrite filter posts with pattern mathing methods

--- lib/blog/datasets/posts/services/filter_posts.ex
+++ lib/blog/datasets/posts/services/filter_posts.ex
@@ -13,26 +13,34 @@ defmodule Blog.Datasets.Posts.Services.FilterPosts do
     Enum.reduce(
       Map.keys(params),
       query,
-      fn filter, acc_query -> apply_filter(acc_query, filter, params[filter]) end
+      fn filter, acc_query -> apply_filter(acc_query, params[filter], %{filter: filter}) end
     )
   end
 
-  defp apply_filter(query, filter, value) do
-    case filter do
-      "title" ->
-        from(p in query, where: ilike(p.title, ^"%#{value}%"))
-      "status" ->
-        from(p in query, where: p.status == ^value)
-      "category_ids" ->
-        from(p in query,
-          join: cat in assoc(p, :categories),
-          where: cat.id in ^value)
-      "created_at_from" ->
-        from(p in query, where: p.inserted_at >= ^value)
-      "created_at_to" ->
-        from(p in query, where: p.inserted_at < ^value)
-      _ ->
-        query
-    end
+  defp apply_filter(query, value, %{filter: "title"}) do
+    from(p in query, where: ilike(p.title, ^"%#{value}%"))
+  end
+
+  defp apply_filter(query, value, %{filter: "status"}) do
+    from(p in query, where: p.status == ^value)
+  end
+
+  defp apply_filter(query, value, %{filter: "category_ids"}) do
+    from(p in query,
+         join: cat in assoc(p, :categories),
+         where: cat.id in ^value)
+  end
+  
+  defp apply_filter(query, value, %{filter: "created_at_from"}) do
+    from(p in query, where: p.inserted_at >= ^value)
+  end
+
+
+  defp apply_filter(query, value, %{filter: "created_at_to"}) do
+    from(p in query, where: p.inserted_at < ^value)
+  end
+
+  defp apply_filter(query, _value, _filter) do
+    query
   end
 end
