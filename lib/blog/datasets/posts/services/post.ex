601cd79976ebe72e426e28894dc3f3a28fb9c4c4
Maks <maks.shitko@mail.ru>
2020-12-09 23:52:33 +0300

    allow attach categories to post in changeset

--- lib/blog/datasets/posts/services/post.ex
+++ lib/blog/datasets/posts/services/post.ex
@@ -3,6 +3,8 @@ defmodule Blog.Datasets.Posts.Post do
   import Ecto.Changeset
 
   alias Blog.Datasets.PostsCategories.PostsCategory
+  alias Blog.Datasets.Categories.Category
+  alias Blog.Repo
 
   schema "posts" do
     field :published_at, :naive_datetime
@@ -11,16 +13,20 @@ defmodule Blog.Datasets.Posts.Post do
     field :title, :string
 
     has_many :posts_categories, PostsCategory, on_delete: :delete_all
-    has_many :categories, through: [:posts_categories, :category]
+    many_to_many :categories, Category, join_through: "posts_categories", on_replace: :delete
 
     timestamps()
   end
 
-  # def changeset(post, )
-
-  @doc false
-  def changeset(post, attrs) do
+  def changeset(post, %{category_ids: category_ids} = attrs) do
     post
+    |> Repo.preload(:categories)
+    |> changeset(Map.delete(attrs, :category_ids))
+    |> put_assoc(:categories, Enum.map(category_ids, fn id -> %Category{id: id} end))
+  end
+
+  def changeset(post, attrs) do
+    changeset = post
     |> cast(attrs, [:title, :text, :status, :published_at])
     |> validate_required([:title, :text, :status])
     |> validate_inclusion(:status, ["draft", "published", "require_moderation"])
